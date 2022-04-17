c8e5d92b10f75516578b2631bfa4d367a4dd0f5f
Nafisa06 <nafisa.ila@gmail.com>
2022-04-18 00:47:38 +1000

    code to remove and add reactions

--- Controller/Thought-Controller.js
+++ Controller/Thought-Controller.js
@@ -83,3 +83,34 @@ removeThought({ params }, res) {
         })
         .catch(err => res.json(err));
 },
+
+// add reaction
+addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+        { _id: params.thoughtId },
+        { $addToSet: { reactions: body } },
+        { new: true, runValidators: true }
+    )
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                return res.status(404).json({ message: 'No thought found with this id!' });
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.json(err));
+},
+
+// remove reaction
+removeReaction({ params }, res) {
+    console.log(params.thoughtId, params.reactionId);
+    Thought.findOneAndUpdate(
+        { _id: params.thoughtId },
+        { $pull: { reactions: { reactionId: params.reactionId } } },
+        { runValidators: true, new: true }
+    )
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => res.json(err));
+}
+};
+
+module.exports = thoughtController;
