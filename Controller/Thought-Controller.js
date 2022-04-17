a42f63315aa2701e71ce14efae6bce565af9ce53
Nafisa06 <nafisa.ila@gmail.com>
2022-04-18 00:35:22 +1000

    code for getting the user by id

--- Controller/Thought-Controller.js
+++ Controller/Thought-Controller.js
@@ -12,4 +12,19 @@ const thoughtController = {
                 res.status(400).json(err);
             });
     },
-    
+
+// get one user by id
+    getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.thoughtId })
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found with this id!' });
+                    return;
+                }
+                res.json(dbThoughtData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
